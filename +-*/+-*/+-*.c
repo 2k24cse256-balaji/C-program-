d4d3157c8d9cb10df5b9a64149363588d813c386
2k24cse256-balaji <2k24cse256@kiot.ac.in>
2026-02-06 14:36:29 +0530

    Add basic arithmetic operations in new C file

---
+++ +-*/+-*/+-*.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b;
+
+    // Read inputs
+
+    scanf("%d", &a);
+
+    scanf("%d", &b);
+
+    // Perform operations
+
+    printf("Sum: %d\n", a + b);
+
+    printf("Difference: %d\n", a - b);
+
+    printf("Product: %d\n", a * b);
+
+    printf("Quotient: %d\n", a / b);
+
+    printf("Remainder: %d\n", a % b);
+
+    return 0;
+
+}
